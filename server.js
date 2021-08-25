616e1d8d08f2d227b2a66713a5e00b018d59ef2f
Matias Guerrero <matias.f.guerrero19@gmail.com>
2021-08-25 00:20:48 -0300

    clase 09: ejemplo de res object

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const port = 3000;
 const hostname = 'localhost';
-
+//imprimimos por pantalla del navegador el mensaje: Wrlcome to Mafius server
+//para ello ejecuta server.js con nodemon
 const server = http.createServer((req, res) => {
-    console.log('request url', req.url);
-    console.log('request method', req.method);
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write('Welcome to Mafius server');
+    res.setHeader('Content-Type','text/html');
+    res.write('<body class="app"></body>');
+    res.write('<h3>Welcome to Mafius server</h3>');
+    res.write('<p>This is Node.js course</p>');
+    res.end();
 });
 
 server.listen(port, hostname,() => {
